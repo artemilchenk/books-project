c43abc257b0e33f1fa71d36e1a6ed82d37376109
Artem Ilchenko <artem.ilchenko@plus4u.net>
2022-10-20 09:49:30 +0300

    some

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import styles from './App.module.css'
-import {useGetBooks} from "./api/models/book/hook/getBooksHook";
 import {List} from "./components/List/List";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Bookpage} from "./components/BookPage/Bookpage";
 
 
 function App() {
-    const {loading, data, error, getBooks} = useGetBooks()
-
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/book/:id" element={<Bookpage/>}/>
                 <Route index element={<div className={styles.app}>
-                    <List list={data}/>
+                    <List />
                 </div>}/>
             </Routes>
         </BrowserRouter>
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,18 +4,20 @@ import {useEffect, useState} from "react";
 import {Navigate} from "../Navigate/Navigate";
 import {useDispatch, useSelector} from "react-redux";
 import {setSkip} from "../../store/app.slice";
+import {useGetBooks} from "../../api/models/book/hook/getBooksHook";
 
-export function List({list}) {
-    const [books, setBooks] = useState([])
+export function List() {
     const dispatch = useDispatch()
+    const {loading, data, error, getBooks} = useGetBooks()
+    const [books, setBooks] = useState([])
     let skip = useSelector(state => state.app.skip)
     const providerState = useSelector(state => state.app.provider)
     const currencyState = useSelector(state => state.app.currency)
 
     useEffect(() => {
-        setBooks(list?.filter(book => book.provider.includes(providerState)))
-        if(currencyState) setBooks(list?.filter(book => book.provider.includes(providerState) && Object.keys(book.real).includes(currencyState)))
-    }, [list, providerState, currencyState])
+        setBooks(data?.filter(book => book.provider.includes(providerState)))
+        if(currencyState) setBooks(data?.filter(book => book.provider.includes(providerState) && Object.keys(book.real).includes(currencyState)))
+    }, [data, providerState, currencyState])
 
     return (
         <div className={styles.list}>
